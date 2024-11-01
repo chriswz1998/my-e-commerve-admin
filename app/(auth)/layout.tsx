8b3e5d82da5f4c567ebcc27380e2fec1a52b9012
chriswz1998 <scissorwz@gmail.com>
2024-10-31 22:39:54 -0700

    add login bg color:)

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,7 +2,9 @@ import { ReactNode } from 'react'
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <div className={'w-full h-full flex items-center justify-center'}>
+    <div
+      className={'w-full h-full flex items-center justify-center bg-blue-50'}
+    >
       {children}
     </div>
   )
